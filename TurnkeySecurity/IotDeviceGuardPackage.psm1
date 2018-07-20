c2664a39ff0c6dbe75aff16a19110c058f5bcdc8
Andrew Phelps <anphel@microsoft.com>
2018-07-20 16:44:16 -0700

    download db_MSFTproductionWindowsSigningCA2011.cer instead of including in repository

--- TurnkeySecurity/IotDeviceGuardPackage.psm1
+++ TurnkeySecurity/IotDeviceGuardPackage.psm1
@@ -2,11 +2,13 @@
 Import-Module $PSScriptRoot\IoTDeviceGuardBitLocker.psm1 -Force
 Import-Module $PSScriptRoot\IoTDeviceGuardSIPolicy.psm1 -Force
 Import-Module $PSScriptRoot\IoTDeviceGuardOEMCustomization.psm1 -Force
+Import-Module $PSScriptRoot\IoTDeviceGuardUtils.psm1 -Force
 
 $ErrorActionPreference = 'stop'
 
 function New-IotDeviceGuardPackage([string]$ConfigFileName)
 {
+    DownloadProductionWindowsCert -ConfigFileName $ConfigFileName
     New-IoTSecureBootPackage -ConfigFileName $ConfigFileName
     New-IoTBitLockerPackage -ConfigFileName $ConfigFileName
     New-IoTSIPolicyPackage -ConfigFileName $ConfigFileName
