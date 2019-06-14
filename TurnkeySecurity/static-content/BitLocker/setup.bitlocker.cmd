ba5e2f203d77eb860e3f35d77c52be30c1656914
Paul Monson <paulmon@microsoft.com>
2019-06-14 09:30:54 -0700

    comment out wait logic

--- TurnkeySecurity/static-content/BitLocker/setup.bitlocker.cmd
+++ TurnkeySecurity/static-content/BitLocker/setup.bitlocker.cmd
@@ -8,26 +8,26 @@ if %errorlevel% == 1 (
     goto :end
 )
 
-:: Enable below scripts to wait until the bit locker encryption completed on the device.
-reg query HKLM\Software\IoT /v DeviceGuardBitlockerEncryptionComplete >nul 2>&1
+REM :: Enable below scripts to wait until the bit locker encryption completed on the device.
+REM reg query HKLM\Software\IoT /v DeviceGuardBitlockerEncryptionComplete >nul 2>&1
 
-if %errorlevel% == 0 ( goto :end )
+REM if %errorlevel% == 0 ( goto :end )
 
-echo.	>>%systemdrive%\bitlockerstatus.log 2>&1
-echo Waiting for bit locker encryption to complete....	>>%systemdrive%\bitlockerstatus.log 2>&1
-echo started @ %DATE% %TIME%	>>%systemdrive%\bitlockerstatus.log 2>&1
-%windir%\system32\sectask.exe -waitencryptcomplete:1800	>nul 2>&1
+REM echo.	>>%systemdrive%\bitlockerstatus.log 2>&1
+REM echo Waiting for bit locker encryption to complete....	>>%systemdrive%\bitlockerstatus.log 2>&1
+REM echo started @ %DATE% %TIME%	>>%systemdrive%\bitlockerstatus.log 2>&1
+REM %windir%\system32\sectask.exe -waitencryptcomplete:1800	>nul 2>&1
 
-if %errorlevel% == 0 (
-	reg add HKLM\Software\IoT /v DeviceGuardBitlockerEncryptionComplete /t REG_DWORD /d 1 /f >nul 2>&1
-	echo Device bit locker encryption completed successfully.	>>%systemdrive%\bitlockerstatus.log 2>&1
-	echo ended @ %DATE% %TIME%	>>%systemdrive%\bitlockerstatus.log 2>&1
-    goto :end )
+REM if %errorlevel% == 0 (
+REM 	reg add HKLM\Software\IoT /v DeviceGuardBitlockerEncryptionComplete /t REG_DWORD /d 1 /f >nul 2>&1
+REM 	echo Device bit locker encryption completed successfully.	>>%systemdrive%\bitlockerstatus.log 2>&1
+REM 	echo ended @ %DATE% %TIME%	>>%systemdrive%\bitlockerstatus.log 2>&1
+REM     goto :end )
 
-if %errorlevel% == -2147023436 (
-	echo Device bit locker encryption timed out.	>>%systemdrive%\bitlockerstatus.log 2>&1
-    goto :end )
+REM if %errorlevel% == -2147023436 (
+REM 	echo Device bit locker encryption timed out.	>>%systemdrive%\bitlockerstatus.log 2>&1
+REM     goto :end )
 
-echo Error %errorlevel% occurred in bit locker encryption.	>>%systemdrive%\bitlockerstatus.log 2>&1
+REM echo Error %errorlevel% occurred in bit locker encryption.	>>%systemdrive%\bitlockerstatus.log 2>&1
 
 :end
